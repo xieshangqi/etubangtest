7697a54958d474208a1edaea17e91dc5b2895e4b
Administrator <yuanguoweiSM@163.com>
2021-05-31 17:39:32 +0800

    renren测试

--- src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -51,7 +51,7 @@ public class SysLoginController extends AbstractController {
 		response.setHeader("Cache-Control", "no-store, no-cache");
 		response.setContentType("image/jpeg");
 
-		//获取图片验证码
+		//获取图片验
 		BufferedImage image = sysCaptchaService.getCaptcha(uuid);
 
 		ServletOutputStream out = response.getOutputStream();
